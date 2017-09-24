9117cc469c828a85fb2839c3dfc0774be9a5f97f
vagrant <nethra.madesh@gmail.com>
2017-09-24 08:21:21 +0000

    latest changes to couchbase

--- server.js
+++ server.js
@@ -1,34 +1,87 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
 var app = express();
 var bodyParser = require('body-parser')
 var couchbase = require('couchbase')
 var cluster = new couchbase.Cluster('couchbase://localhost/');
 var bucket = cluster.openBucket('default');
 var N1qlQuery = couchbase.N1qlQuery;
+const saltRounds = 10;
 app.use(bodyParser.json({limit:'100kb'}));
 
 
 app.get('/usercredential', function(req, res){
   bucket.manager().createPrimaryIndex(function() {
- console.log(req.query.username)
-    bucket.get('user:'+req.query.username, function (err, result) {
-      
-      bucket.query(
-      N1qlQuery.fromString('SELECT pwd FROM default where email = "nethra.madesh@gmail.com" LIMIT 1'),
-      function (err, rows) {
-        //console.log("Got rows: %j", rows);
-        res.send(rows[0].pwd);
-      });
-    });
+ console.log(req.query.email)
+    bucket.get('user:'+req.query.email, function (err, result) {
+      console.log(result.value.password);
+    //  bucket.query(
+      //N1qlQuery.fromString('SELECT pwd FROM default where email = "dinesh3@gmail.com" LIMIT 1'),
+     // function (err, rows) {
+      //  console.log("Got rows: %j", rows);
+        if(result!==null && result.value!==null)
+        {
+        res.send(result.value.password);
+      }
+      else
+      {
+        res.send(null);
+      }
+      //});
+    //});
   });
   
+});
+});
+app.post('/usercredential/validate', function(req, res){
+  bucket.manager().createPrimaryIndex(function() {
+ 
+    bucket.get('user:'+req.body.UserCredential.email, function (err, result) {
+      console.log(result.value.password);
+    //  bucket.query(
+      //N1qlQuery.fromString('SELECT pwd FROM default where email = "dinesh3@gmail.com" LIMIT 1'),
+     // function (err, rows) {
+      //  console.log("Got rows: %j", rows);
+        if(result!==null && result.value!==null)
+        {
+         bcrypt.compare(req.body.UserCredential.password, result.value.password, function(err, result){
+          console.log(result);
+          if(result=="true")
+          {
+            res.send(result);
+          }
+          else
+          {res.statusCode = 401
+
+            res.send("Unauthorized");
+          }
+         }) 
+        
+      }
+      else
+      {
+        res.statusCode = 401
+
+            res.send("Unauthorized");
+      }
+      //});
+    //});
+  });
+  
+});
 });
 
 
 
 app.post('/usercredential', function(req, res){
-  console.log(req.body);
-  bucket.manager().createPrimaryIndex(function() {
+  bcrypt.hash(req.body.UserCredential.password, saltRounds, function(err, hash)
+  {
+if(err==null)
+{
+  req.body.UserCredential.password = hash;
+  console.log(req.body.UserCredential);
+    bucket.manager().createPrimaryIndex(function() {
+
   bucket.upsert('user:'+req.body.UserCredential.email,  req.body.UserCredential
   ,
    function (err, result) {
@@ -38,6 +91,10 @@ app.post('/usercredential', function(req, res){
     }
    });
 });
+  }
+
+  });
+  
 });
 
 /*bucket.manager().createPrimaryIndex(function() {
@@ -57,7 +114,7 @@ app.post('/usercredential', function(req, res){
   
 });*/
 
-app.listen(3000)
+app.listen(3001)
 {
-	console.log("server running on port 3000");
+	console.log("server running on port 3001");
 }
